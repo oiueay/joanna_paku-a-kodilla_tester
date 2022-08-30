0f28aa38791db510c99e19dfba2864a962671e20
Joanna Pakuła <l.0_0.l@wp.pl>
2022-08-30 12:43:50 +0200

    Commit message

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {
+        // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Joanna"); // [3]
+        System.out.println(29); // [3]
+        System.out.println('P'); // [3]
+
+
+    }
 }
+

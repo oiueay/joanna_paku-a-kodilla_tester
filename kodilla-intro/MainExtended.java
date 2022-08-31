5ac4d4f92363887e056de2a35f954ef7d12eb8ea
Joanna Pakuła <l.0_0.l@wp.pl>
2022-08-31 12:07:23 +0200

    Initial version

---
+++ kodilla-intro/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+    public static void main(String[] args) {
+
+        String catName;
+        catName = "Tygrys";
+
+        char firstLetterOfName;
+        firstLetterOfName = 'T';
+
+        int catBirth;
+        catBirth = 2010;
+
+        double catWeight;
+        catWeight = 4.5;
+
+        boolean isLovely;
+        isLovely = true;
+    }
+    }

77dcedce218651634176261aa7b628f95d5df686
Joanna Pakuła <l.0_0.l@wp.pl>
2022-09-24 21:12:14 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/Colour/Colour.java
@@ -0,0 +1,33 @@
+package Colour;
+
+import java.util.Scanner;
+
+public class Colour {
+
+    public static String getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select a letter (o- orange, b- black, r- red):");
+            String letter = scanner.nextLine().trim();
+            switch (letter) {
+                case "o":
+                    return "orange";
+                case "b":
+                    return "black";
+                case "r":
+                    return "red";
+                default:
+                    System.out.println("Wrong choice. Try again.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String result = Colour.getUserSelection();
+        System.out.println("Result: " + result);
+    }
+}
+
+
+
+

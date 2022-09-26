894b436d612a4d24df9c7be62d2125cb9bdb49bf
Joanna Pakuła <l.0_0.l@wp.pl>
2022-09-26 11:35:36 +0200

    Initial version

--- kodilla-intro/src/main/java/Refakturyzacja/Obiektowy.java
+++ kodilla-intro/src/main/java/Refakturyzacja/Obiektowy.java
@@ -1,2 +1,29 @@
-package Refakturyzacja;public class Obiektowy {
+package Refakturyzacja;
+
+public class Obiektowy {
+    String name;
+    double age;
+    double height;
+
+    Obiektowy(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void checkPerson() {
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Obiektowy person = new Obiektowy("Adam", 40.5, 178);
+        System.out.println(person.name + person.age + person.height);
+        person.checkPerson();
+    }
 }

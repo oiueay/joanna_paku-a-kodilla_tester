0ab78583691d6249a897fa02aca8fe791bd23f6f
Joanna Pakuła <l.0_0.l@wp.pl>
2022-09-29 13:02:41 +0200

    Initial version

--- kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/abstracts/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/abstracts/Application.java
@@ -3,10 +3,10 @@ package com.kodilla.com.kodilla.abstracts;
 public class Application {
     public static void main(String[] args) {
         Dog dog = new Dog(); //Dog dog = new Dog();
-//        dog.giveVoice();
+        dog.giveVoice();
 
-//        Duck duck = new Duck();
-//        duck.giveVoice();
+        Duck duck = new Duck();
+        duck.giveVoice();
 
         AnimalProcessor processor = new AnimalProcessor();
         processor.process(dog);

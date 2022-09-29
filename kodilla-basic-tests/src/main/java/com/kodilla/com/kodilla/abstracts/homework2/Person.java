f5b144db664cc84c8f4c5273cbc06fbdb23df9f4
Joanna Pakuła <l.0_0.l@wp.pl>
2022-09-29 13:05:11 +0200

    Initial version

--- kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/abstracts/homework2/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/abstracts/homework2/Person.java
@@ -1,2 +1,21 @@
-package com.kodilla.com.kodilla.abstracts.homework2;public class Person {
+package com.kodilla.com.kodilla.abstracts.homework2;
+
+import com.kodilla.com.kodilla.abstracts.Animal;
+
+public class Person {
+
+    String firstName;
+    int age;
+    String job;
+
+    Person(String firstName, int age, String job) {
+        this.firstName = firstName;
+        this.age = age;
+        this.job = job;
+    }
+
+    public void responsibilities(Job job) {
+        System.out.println(job.getResponsibilities());
+    }
 }
+

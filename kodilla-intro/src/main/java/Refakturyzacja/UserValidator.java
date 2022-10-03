1610a3868d7aef825d3d796846c2d90b6216fd26
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-03 12:43:26 +0200

    change

--- kodilla-intro/src/main/java/Refakturyzacja/UserValidator.java
+++ kodilla-intro/src/main/java/Refakturyzacja/UserValidator.java
@@ -1,2 +1,21 @@
-package Refakturyzacja;public class UserValidator {
+package Refakturyzacja;
+
+public class UserValidator {
+
+    String name;
+    double age;
+    double height;
+
+    void validateName(String name) {
+        if (name != null) {
+        }
+    }
+
+    void validateAgeAndHeight(double age, double height) {
+        if (this.age > 30 && this.height > 160) {
+            System.out.println("User is older than 30 and taller than 160cm");
+        } else {
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+        }
+    }
 }

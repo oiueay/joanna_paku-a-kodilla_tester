03f3c9a41870d3830baabb9acfba0179e3d866d1
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-04 16:56:17 +0200

    Initial version

--- kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/basic_assertion2/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/basic_assertion2/Calculator.java
@@ -1,2 +1,24 @@
-package com.kodilla.com.kodilla.basic_assertion2;public class Calculator {
+package com.kodilla.com.kodilla.basic_assertion2;
+
+public class Calculator {
+
+    public int subtract(int a, int b) {
+
+        return (a - b);
+    }
+
+    public double exponentiation(double a, double n) {
+
+        return Math.pow(a, n);
+    }
+
+    public double exponentiation2(double aa, double n) {
+
+        return Math.pow(aa, n);
+    }
+
+    public double exponentiation3(double aaa, double n) {
+
+        return Math.pow(aaa, n);
+    }
 }

29436e00d2da99637172485b7ebb2119e193002a
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-04 16:54:28 +0200

    Initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/com/kodilla/basic_assertion2/Calculator.java
@@ -0,0 +1,2 @@
+package com.kodilla.com.kodilla.basic_assertion2;public class Calculator {
+}
---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion2/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.basic_assertion2;
+
+import com.kodilla.com.kodilla.basic_assertion2.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        double n = 2;
+        double aa = -0.1;
+        double aaa = 0;
+
+        int subtractResult = calculator.subtract(a, b);
+        assertEquals(-3, subtractResult);
+
+        double exponentiationResult = calculator.exponentiation(a, n);
+        assertEquals (25, exponentiationResult, 0.1);
+
+        double exponentiationResult2 = calculator.exponentiation2(aa, n);
+        assertEquals (-0.01, exponentiationResult2, 0.1);
+
+        double exponentiationResult3 = calculator.exponentiation3(aaa, n);
+        assertEquals (0, exponentiationResult3, 0.1);
+    }
+}

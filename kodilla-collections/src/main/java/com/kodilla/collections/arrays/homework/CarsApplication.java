91019ca7baef8293213f909827b6f7832ca8ca65
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-07 12:55:58 +0200

    Initial version

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -1,2 +1,29 @@
-package com.kodilla.collections.arrays.homework;public class CarsApplication {
+package com.kodilla.collections.arrays.homework;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Opel;
+import com.kodilla.collections.interfaces.homework.Skoda;
+import java.util.Random;
+
+public class CarsApplication {
+
+    public static void main(String[] args) {
+        Car[] cars = new Car[15 + 1];
+        for (int n = 0; n < cars.length; n++)
+            cars[n] = drawCar();
+        for (Car car : cars)
+            CarUtils.describeCar(car);
+    }
+
+    public static Car drawCar() {
+        Random random = new Random();
+        int drawnCarKind = random.nextInt(15 + 1);
+        if (drawnCarKind == 1)
+            return new Skoda();
+        else if (drawnCarKind == 2)
+            return new Opel();
+        else {
+            return new Ford();
+        }
+    }
 }

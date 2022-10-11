cc6d7bad0d47ee803b75361910252c18df265ce8
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-11 09:38:56 +0200

    Initial version

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -1,2 +1,34 @@
-package com.kodilla.collections.lists.homework;public class CarsListApplication {
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Skoda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+
+    public static void main(String[] args) {
+        List<Skoda> cars = new ArrayList<>();
+        Skoda Octavia = new Skoda();
+        Skoda Superb = new Skoda();
+        Skoda Kamiq = new Skoda();
+        cars.add(Octavia);
+        cars.add(Superb);
+        cars.add(Kamiq);
+
+        for (Skoda car : cars) {
+            CarUtils.describeCar(car);
+        }
+
+        cars.remove(0);
+        cars.remove(Kamiq);
+
+        System.out.println(cars.size());
+
+
+        for (Skoda car : cars) {
+            CarUtils.describeCar(car);
+        }
+    }
 }

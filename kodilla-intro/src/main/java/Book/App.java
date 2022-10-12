359aac51472b694714bd06a180fb85cbf11cb085
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-12 10:25:21 +0200

    change

--- kodilla-intro/src/main/java/Book/App.java
+++ kodilla-intro/src/main/java/Book/App.java
@@ -1,2 +1,9 @@
-package Book;public class App {
+package Book;
+
+public class App {
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+        System.out.println(book.getAuthor());
+        System.out.println(book.getTitle());
+    }
 }

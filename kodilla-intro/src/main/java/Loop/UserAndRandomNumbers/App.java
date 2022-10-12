3e66a34d04dab25c0bff60a413bd6db85db97c11
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-12 11:58:59 +0200

    change

--- kodilla-intro/src/main/java/Loop/UserAndRandomNumbers/App.java
+++ kodilla-intro/src/main/java/Loop/UserAndRandomNumbers/App.java
@@ -1,2 +1,30 @@
-package Loop.UserAndRandomNumbers;public class App {
+package Loop.UserAndRandomNumbers;
+
+import java.util.Random;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        User[] users = new User[100];
+        for (int n = 0; n < 100; n++) {
+            users[n] = new User("User" + n, random.nextInt(40) + 10);
+        }
+
+        double averageAge = calculateAverageAge(users);
+        for (int n = 0; n < users.length; n++) {
+            if (users[n].getAge() > averageAge) {
+                System.out.println(users[n].getName());
+            }
+        }
+    }
+    private static double calculateAverageAge(User[] users) {
+        double sum = 0;
+        for (int n = 0; n < users.length; n++) {
+            sum += users[n].getAge();
+        }
+        return sum / users.length;
+    }
 }
+

2db7086375e11af522719a5e311497b487251127
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-12 10:26:02 +0200

    change

--- kodilla-intro/src/main/java/Book/Book.java
+++ kodilla-intro/src/main/java/Book/Book.java
@@ -1,19 +1,24 @@
 package Book;
 
-public class Book {
+class Book {
 
-    private static String author;
-    private static String title;
+    private String author;
+    private String title;
 
-
-    static String of(String author, String title) {
-        Book.author = author;
-        Book.title = title;
-        return author + title;
+    private Book(String author, String title) {
+        this.author = author;
+        this.title = title;
     }
 
-    public static void main(String[] args) {
-        String name = Book.of("Isaac Asimov", "The Galaxy");
-        System.out.println(name);
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }

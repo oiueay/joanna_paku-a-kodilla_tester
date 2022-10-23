b9178b15201cef5c9613416b8b7392d47da30320
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-23 21:25:52 +0200

    Initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -1,2 +1,29 @@
-package com.kodilla.collections.adv.exercises.homework;public class FlightFinder {
+package com.kodilla.collections.adv.exercises.homework;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightFinder {
+
+    public static void main(String[] args) {
+        List<String> FlightsFrom = findFlightsFrom("MIA");
+        System.out.println(FlightsFrom);
+    }
+
+    private static List<String> findFlightsFrom(String departure) {
+        List<Flight> nameofdeparture = FlightRepository.getFlightsTable()
+                .stream()
+                .filter((f -> f.getDeparture().equals("MIA")));
+                .map(Flight :: getDeparture)
+                .collect(Collectors.toList());
+        return nameofdeparture;
+    }
+
+    public static String getDeparture(Flight departure) {
+        return departure.getDeparture();
+    }
 }
+
+
+
+
+

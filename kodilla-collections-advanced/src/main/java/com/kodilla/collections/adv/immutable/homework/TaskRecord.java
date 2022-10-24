2245614337a79ac497dabbca413cb4b2d0d0a9ff
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-24 12:22:56 +0200

    change

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskRecord.java
@@ -1,5 +1,4 @@
-//package com.kodilla.collections.adv.immutable.homework;
-//public record TaskRecord(String title, int duration) {
-//
-//  // String name; //instance field id not allowed in record
-//}
+package com.kodilla.collections.adv.immutable.homework;
+
+public record TaskRecord(String title, int duration) {
+}

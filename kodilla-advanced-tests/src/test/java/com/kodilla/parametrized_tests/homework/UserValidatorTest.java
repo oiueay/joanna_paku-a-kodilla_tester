173d2c3430a79fc8e046eeba3e74a64c4c994de5
Joanna Pakuła <l.0_0.l@wp.pl>
2022-10-31 15:41:33 +0100

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTest.java
@@ -1,4 +1,25 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserValidatorTest {
-  
+
+    private UserValidator validator = new UserValidator();
+
+    @ParameterizedTest
+    @CsvSource(value = {"AlaMakota"})
+    public void checkIfUsernameFulfilsTheConditionOfPattern(String username) {
+        boolean result = validator.validateUsername(username);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"ala.makota@gmail.com"})
+    public void checkIfUsernameFulfilsTheConditionOfPattern2(String email) {
+        boolean result = validator.validateEmail(email);
+        assertTrue(result);
+    }
 }

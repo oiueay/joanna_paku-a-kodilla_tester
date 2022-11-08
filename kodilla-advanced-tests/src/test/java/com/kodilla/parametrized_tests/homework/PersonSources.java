1d60b00f1ec7abae04dbc9f597f0939bb03287dc
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-08 17:15:02 +0100

    Change

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
@@ -2,22 +2,20 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.provider.Arguments;
 import java.util.stream.Stream;
 
-
-
 public class PersonSources {
     static Stream<Arguments> provideBMI() {
         return Stream.of(
-                Arguments.of( 43, 170),
-                Arguments.of( 44, 170),
-                Arguments.of( 53, 170),
-                Arguments.of( 70, 170),
-                Arguments.of( 85, 170),
-                Arguments.of( 100, 170),
-                Arguments.of( 115, 170),
-                Arguments.of( 140, 170),
-                Arguments.of( 165, 170),
-                Arguments.of( 180, 170)
-
+                Arguments.of( 43, 170, "Very severely underweight"),
+                Arguments.of( 44, 170, 15.22),
+                Arguments.of( 53, 170, 18.34),
+                Arguments.of( 70, 170, 24.22),
+                Arguments.of( 85, 170, 29.41),
+                Arguments.of( 100, 170, 34.6),
+                Arguments.of( 115, 170, 39.79),
+                Arguments.of( 130, 170, 44.98),
+                Arguments.of( 140, 170, 48.44),
+                Arguments.of( 165, 170, 57.09),
+                Arguments.of( 180, 170, 62.28)
         );
     }
 }

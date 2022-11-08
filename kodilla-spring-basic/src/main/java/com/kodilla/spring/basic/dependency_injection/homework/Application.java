6dd1c1bef0902e72379528b7b3191da2ebcf3d92
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-08 19:28:08 +0100

    Change

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -5,7 +5,7 @@ public class Application {
     public static void main(String[] args) {
         DeliveryServiceInterface deliveryService = new DeliveryService();
         NotificationServiceInterface notificationService = new NotificationService();
-        ShippingCenter shippingCenter = new ShippingCenter((DeliveryService) deliveryService, (NotificationService) notificationService);
+        ShippingCenter shippingCenter = new ShippingCenter(deliveryService, notificationService);
 
         shippingCenter.sendPackage("Hill Street 11, New York", 18.2);
     }

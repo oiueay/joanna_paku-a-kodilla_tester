9a2510ecbaf014691620f487d2d808c2b58928ad
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-08 12:26:28 +0100

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
@@ -1,2 +1,23 @@
-package com.kodilla.parametrized_tests.homework;public class PersonSources {
+package com.kodilla.parametrized_tests.homework;
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
+
+
+
+public class PersonSources {
+    static Stream<Arguments> provideBMI() {
+        return Stream.of(
+                Arguments.of( 43, 170),
+                Arguments.of( 44, 170),
+                Arguments.of( 53, 170),
+                Arguments.of( 70, 170),
+                Arguments.of( 85, 170),
+                Arguments.of( 100, 170),
+                Arguments.of( 115, 170),
+                Arguments.of( 140, 170),
+                Arguments.of( 165, 170),
+                Arguments.of( 180, 170)
+
+        );
+    }
 }

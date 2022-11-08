03057c9def2e91edbd53dc736c706aba923716a0
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-08 12:25:56 +0100

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kodilla.parametrized_tests.homework;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class PersonTest {
-  
+
+    Person bmi1 = new Person();
+
+
+    @ParameterizedTest
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSources#provideBMI")
+    public void shouldProvideBMI(int input, String expected) {
+        assertEquals(expected, bmi1.getBMI());
+    }
 }

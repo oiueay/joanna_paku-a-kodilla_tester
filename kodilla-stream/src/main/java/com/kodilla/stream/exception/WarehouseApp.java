ca89d36784c3018f421f246bf939e04607448f53
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-10 13:32:37 +0100

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/exception/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/WarehouseApp.java
@@ -1,2 +1,16 @@
-package com.kodilla.stream.exception;public class WarehouseApp {
+package com.kodilla.stream.exception;
+
+public class WarehouseApp {
+
+    public static void main(String[] args) {
+         Warehouse warehouse = new Warehouse();
+         warehouse.addOrder(new MyOrder("1234"));
+         warehouse.addOrder(new MyOrder("5678"));
+
+        try {
+            warehouse.getOrder("9999");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Brak zamówienia o podanym numerze");
+        }
+    }
 }

43f6f0ae939ae45738eb492639106ac8a5cac0c3
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-10 13:32:09 +0100

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/exception/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/Warehouse.java
@@ -1,2 +1,27 @@
-package com.kodilla.stream.exception;public class Warehouse {
+
+package com.kodilla.stream.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Warehouse {
+
+    private List<MyOrder> orders = new ArrayList<>();
+
+    public void addOrder(MyOrder order){
+        orders.add(order);
+    }
+
+    public MyOrder getOrder(String number) throws OrderDoesntExistException {
+        Optional<MyOrder> result = orders.stream()
+                .filter(o -> o.getNumber().equals(number))
+                .findFirst();
+        if (result.isEmpty()) {
+            throw new OrderDoesntExistException();
+        }
+        return result.get();
+    }
 }
+
+

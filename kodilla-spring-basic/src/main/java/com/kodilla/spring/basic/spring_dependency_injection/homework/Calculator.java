f61f5d42614b7d9e5100bab6f373485eb4c1681d
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-15 12:48:40 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -1,2 +1,33 @@
-package com.kodilla.spring.basic.spring_dependency_injection.homework;public class Calculator {
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    private Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+//tutaj: "każda metoda powinna przed zwróceniem wyniku wywołać metodę display(double value) obiektu display"
+
+
+    private double add(double a, double b) {
+        return a + b;
+    }
+
+    private double subtract(double a, double b) {
+        return a - b;
+    }
+
+    private double multiply(double a, double b) {
+        return a * b;
+    }
+
+    private double divide(double a, double b) {
+        return a / b;
+    }
 }
+

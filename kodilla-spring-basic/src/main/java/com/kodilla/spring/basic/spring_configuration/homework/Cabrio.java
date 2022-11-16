7a816fe1abf61a15bc20f7e61d5188f81b2bfdd3
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-16 21:23:08 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Cabrio.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Cabrio.java
@@ -1,2 +1,15 @@
-package com.kodilla.spring.basic.spring_configuration.homework;public class Cabrio {
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+public class Cabrio implements Car{
+
+
+    @Override
+    public boolean hasHeadlightsTurnedOn() {
+        return false;
+    }
+
+    @Override
+    public String getCarType() {
+        return "Cabrio";
+    }
 }

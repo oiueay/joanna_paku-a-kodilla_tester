c52f2a2a3bd3e60a0024201c989fc0510c90d57e
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-16 13:35:24 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/ShippingCenter.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/ShippingCenter.java
@@ -1,2 +1,21 @@
-package com.kodilla.spring.basic.spring_dependency_injection.homework2;public class ShippingCenter {
+package com.kodilla.spring.basic.spring_dependency_injection.homework2;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class ShippingCenter {
+
+    @Resource(name = "deliveryService")
+    private DeliveryServiceInterface deliveryServiceInterface;
+    @Resource(name = "notificationService")
+    private NotificationServiceInterface notificationServiceInterface;
+
+    public String sendPackage(String address, double weight) {
+        if (deliveryServiceInterface.deliverPackage(address, weight)) {
+            return notificationServiceInterface.success(address);
+        }
+        return notificationServiceInterface.fail(address);
+    }
 }

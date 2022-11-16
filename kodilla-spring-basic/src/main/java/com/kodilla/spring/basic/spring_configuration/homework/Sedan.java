a4f762c91bdb0de87f0889122ebe1cb13b4db137
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-16 21:23:41 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Sedan.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Sedan.java
@@ -1,2 +1,15 @@
-package com.kodilla.spring.basic.spring_configuration.homework;public class Sedan {
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+public class Sedan implements Car{
+
+
+    @Override
+    public boolean hasHeadlightsTurnedOn() {
+        return false;
+    }
+
+    @Override
+    public String getCarType() {
+        return "Sedan";
+    }
 }

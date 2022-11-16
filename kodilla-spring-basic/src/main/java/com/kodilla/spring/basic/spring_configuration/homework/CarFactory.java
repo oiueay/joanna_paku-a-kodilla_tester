57e22c4813454478bea54797aede04b15d329202
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-16 21:22:34 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -1,2 +1,29 @@
-package com.kodilla.spring.basic.spring_configuration.homework;public class CarFactory {
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+import java.util.Scanner;
+
+@Configuration
+public class CarFactory {
+
+    @Bean
+    public Car randomCar() {
+        Car car;
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.nextLine();
+        if (season == "Winter") {
+            car = new SUV();
+        } else if (season == "Summer") {
+            car = new Cabrio();
+        } else if (season == "Autumn" || season == "Spring") {
+            car = new Sedan();
+        } else {
+            System.out.println("Type season of the year");
+        }
+        return car;
+    }
 }
+

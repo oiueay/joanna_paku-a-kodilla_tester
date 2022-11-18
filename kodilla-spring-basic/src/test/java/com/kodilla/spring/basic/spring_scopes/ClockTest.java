0df068afa16248459b4921507fe932161d848630
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-18 14:51:16 +0100

    Initial version

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/ClockTest.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/ClockTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kodilla.spring.basic.spring_scopes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 class ClockTest {
-  
+
+    @Test
+    public void shouldCreateDifferentTasks() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Clock firstBean = context.getBean(Clock.class);
+        Clock secondBean = context.getBean(Clock.class);
+        Clock thirdBean = context.getBean(Clock.class);
+        Assertions.assertNotEquals(firstBean, secondBean);
+        Assertions.assertNotEquals(secondBean, thirdBean);
+        Assertions.assertNotEquals(firstBean, thirdBean);
+        System.out.println(firstBean.now());
+        System.out.println(secondBean.now());
+        System.out.println(thirdBean.now());
+    }
 }
+

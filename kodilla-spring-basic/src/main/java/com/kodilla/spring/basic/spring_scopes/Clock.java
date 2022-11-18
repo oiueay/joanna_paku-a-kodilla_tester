96b42fcfef650c4668f3c1f0d7b059f231263382
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-18 14:50:52 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/Clock.java
@@ -3,21 +3,15 @@ package com.kodilla.spring.basic.spring_scopes;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 
 @Component
 @Scope("prototype")
 public class Clock {
 
-    private LocalTime time;
-
-    public Clock(LocalTime time) {
-        this.time = time;
-    }
-
-    LocalTime getTime() {
-        return time;
+    LocalDateTime now() {
+        return LocalDateTime.now();
     }
 }
 

f0ae5ba2f0da67f95e1b7be18620e270f652f7fc
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-25 13:07:58 +0100

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,10 +1,26 @@
-//package com.kodilla.stream.homework;
-//
-//import com.kodilla.stream.UsersRepository;
-//
-//public class ForumStats {
-//    public static void main(String[] args) {
-//
-//        double avg = UsersRepository.getUserList()
-//    }
-//}
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+
+import java.util.List;
+
+public class ForumStats {
+
+    public static double getAvgPostsOver40(List<User> users) {
+        return users.stream()
+                .filter(u -> u.getAge() >= 40)
+                .mapToInt(u -> u.getNumberOfPost())
+                .average()
+                .getAsDouble();
+    }
+
+    public static double getAvgPostsUnder40(List<User> users) {
+        return users.stream()
+                .filter(u -> u.getAge() < 40)
+                .mapToInt(u -> u.getNumberOfPost())
+                .average()
+                .getAsDouble();
+    }
+}
+
+

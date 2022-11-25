30ea1c3f00a17e579267aab2007c321bcbae7cf3
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-25 13:18:20 +0100

    Initial version

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -10,24 +10,24 @@ class ForumStatsTest {
 
     private List<User> allUsers() {
         return List.of(
-                new User("A", 41, 10, "Z"),
-                new User("A", 60, 11, "Z"),
-                new User("A", 90, 12, "Z"),
-                new User("A", 38, 13, "Z"),
-                new User("A", 23, 14, "Z"),
-                new User("A", 15, 15, "Z")
+                new User("Walter White", 50, 7, "Chemists"),
+                new User("Jessie Pinkman", 25, 4648, "Sales"),
+                new User("Tuco Salamanca", 34, 116, "Manager"),
+                new User("Gus Firing", 49, 0, "Board"),
+                new User("Gale Boetticher", 44, 2, "Chemists"),
+                new User("Mike Ehrmantraut", 57, 0, "Security")
         );
     }
 
     @Test
     void shouldCalcOver40() {
         double result = ForumStats.getAvgPostsOver40(allUsers());
-        Assertions.assertEquals(11, result);
+        Assertions.assertEquals(2.25, result);
     }
 
     @Test
     void shouldCalcUnder40() {
         double result = ForumStats.getAvgPostsUnder40(allUsers());
-        Assertions.assertEquals(14, result);
+        Assertions.assertEquals(2382, result);
     }
 }

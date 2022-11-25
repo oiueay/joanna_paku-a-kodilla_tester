0d4ceb0ddc61844a0a63c02c7a5f6d2ff6ca802b
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-25 16:44:58 +0100

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -3,26 +3,40 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 @Configuration
 public class CarFactory {
 
     @Bean
     public Car randomCar() {
-        Car car;
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter season of the year:");
-            String season = scanner.nextLine().trim();
-            if (season == "Winter") {
-                return car = new SUV();
-            } else if (season == "Summer") {
-                return car = new Cabrio();
-            } else if (season == "Autumn" || season == "Spring") {
-                return car = new Sedan();
-            }
-            System.out.println("Enter correct season of the year");
+        String season = getSeason(LocalDate.now());
+        boolean lights = isNight(LocalTime.now());
+        if (season.equals("Winter")) {
+            return new SUV(lights);
+        } else if (season.equals("Summer")) {
+            return new Cabrio(lights);
+        } else {
+            return new Sedan(lights);
         }
+    }
+
+    public static boolean isNight(LocalTime time) {
+        return (time.getHour() >= 20 || time.getHour() < 6);
+    }
+
+    public static String getSeason(LocalDate date) {
+        if (date.getMonthValue() < 3) {
+            return "Winter";
+        } else if (date.getMonthValue() < 6) {
+            return "Spring";
+        } else if (date.getMonthValue() < 9) {
+            return "Summer";
+        } else {
+            return "Autumn";
+        }
+
     }
 }

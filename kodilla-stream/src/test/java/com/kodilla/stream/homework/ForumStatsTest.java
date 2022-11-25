b4a7ba46c80ee7e51b626dded2f6ae567dde47bd
Joanna Pakuła <l.0_0.l@wp.pl>
2022-11-25 13:07:26 +0100

    Initial version

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class ForumStatsTest {
-  
+
+    private List<User> allUsers() {
+        return List.of(
+                new User("A", 41, 10, "Z"),
+                new User("A", 60, 11, "Z"),
+                new User("A", 90, 12, "Z"),
+                new User("A", 38, 13, "Z"),
+                new User("A", 23, 14, "Z"),
+                new User("A", 15, 15, "Z")
+        );
+    }
+
+    @Test
+    void shouldCalcOver40() {
+        double result = ForumStats.getAvgPostsOver40(allUsers());
+        Assertions.assertEquals(11, result);
+    }
+
+    @Test
+    void shouldCalcUnder40() {
+        double result = ForumStats.getAvgPostsUnder40(allUsers());
+        Assertions.assertEquals(14, result);
+    }
 }

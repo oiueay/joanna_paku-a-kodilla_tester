71bdc125369bcfee0649c855009a829ff3ceee59
Joanna Pakuła <l.0_0.l@wp.pl>
2022-12-13 13:43:16 +0100

    Initial version

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class AllegroTestingApp {
@@ -19,8 +21,15 @@ public class AllegroTestingApp {
         Alert alert = driver.switchTo().alert();
         alert.dismiss();
 
-        WebElement element = driver.findElement(By.xpath("//*[contains(@class,"mp7g")]/select[0]"));
+        WebElement element = driver.findElement(By.xpath("//*[contains(@class,\"mp7g_oh mr3m_1 s4kyg\")]/select/optgroup/option[3]"));
         Select elementSelect = new Select(element);
-        elementSelect.selectByIndex(2);
+        elementSelect.selectByIndex(3);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className()));
+
+        WebElement inputField = driver.findElement(By.name("string"));
+        inputField.sendKeys("Mavic mini");
+        inputField.submit();
     }
 }

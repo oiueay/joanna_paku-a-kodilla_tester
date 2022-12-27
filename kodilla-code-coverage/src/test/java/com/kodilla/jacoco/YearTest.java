6c429166ed382d1eab397abada5895e95bdb3f5a
Joanna Pakuła <l.0_0.l@wp.pl>
2022-12-27 16:10:01 +0100

    Initial version

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -8,12 +8,29 @@ public class YearTest {
 
     @Test
     public void shouldBeLeapYearIfDivisibleBy400() {
-//given
         Year year = new Year(1600);
-//when
         boolean isLeapYear = year.isLeap();
-//then
         assertTrue(isLeapYear);
     }
 
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4AndNotDivisibleBy100() {
+        Year year = new Year(88);
+        boolean isLeapYear = year.isLeap();
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy4AndDivisibleBy100AndNotDivisibleBy400() {
+        Year year = new Year(100);
+        boolean isLeapYear = year.isLeap();
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy4(){
+        Year year = new Year(75);
+        boolean isLeapYear = year.isLeap();
+        assertFalse(isLeapYear);
+    }
 }

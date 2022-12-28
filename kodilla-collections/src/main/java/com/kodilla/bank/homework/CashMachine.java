d741dcaca77564d60e309ec1a2e3d6beab1e40b4
Joanna Pakuła <l.0_0.l@wp.pl>
2022-12-28 10:42:26 +0100

    Initial version

--- kodilla-collections/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-collections/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -1,54 +1,72 @@
-//package com.kodilla.bank.homework;
-//import java.util.Arrays;
-//
-//public class CashMachine {
-//
-//    private double[] transactions = new double[0];
-//
-//    public void payIn(double value) {
-//        addTransaction(value);
-//    }
-//
-//    public void payOut(double value) {
-//        addTransaction(-value);
-//    }
-//
-//    private void addTransaction(double value) {
-//        transactions = Arrays.copyOf(transactions, transactions.length + 1);
-//        transactions[transactions.length - 1] = value;
-//    }
-//
-//    public int countTransactions() { //liczbę wykonanych transakcji
-//        return transactions.length;
-//    }
-//
-//    public double getSaldo() { // bilans z bankomatu,
-//        double sum = 0;
-//        for (double value : transactions) {
-//            sum += value;
-//        }
-//        return sum;
-//    }
-//
-//    int countPayOutTransaction() {  ///       liczbę transakcji związanych z wypłatą,
-//        return transactions.length - ;
-//    }
-//
-//    int countPayInTransaction() {   //        liczbę transakcji związaną z wpłatą pieniędzy,
-//        return 0;
-//    }
-//
-//    int averageWithdrawal() {    //        średnią wartość wpłaty.
-//        return 0;
-//    }
-//
-////    double averageDeposit() {   //        średnią wartość wypłaty,
-////        double sum = 0;
-////        for (double value : transactions) {
-////            sum += value;
-////        }
-////        return sum/;
-////    }
-//}
-//
-//
+package com.kodilla.bank.homework;
+import java.util.Arrays;
+
+public class CashMachine {
+
+    private double[] transactions = new double[0];
+
+    public void payIn(double value) {
+        addTransaction(value);
+    }
+
+    public void payOut(double value) {
+        addTransaction(-value);
+    }
+
+    private void addTransaction(double value) {
+        transactions = Arrays.copyOf(transactions, transactions.length + 1);
+        transactions[transactions.length - 1] = value;
+    }
+
+    public int countTransactions() { //liczbę wykonanych transakcji
+        return transactions.length;
+    }
+
+    public double getSaldo() { // bilans z bankomatu,
+        double sum = 0;
+        for (double value : transactions) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    int countPayOutTransaction() {  // liczba transakcji związanych z wypłatą,
+        int n=0;
+        for(double value: transactions)
+            if(value<0) n++;
+        return n;
+    }
+
+    int countPayInTransaction() {   // liczba transakcji związana z wpłatą pieniędzy,
+        int n=0;
+        for(double value: transactions)
+            if(value>0) n++;
+        return n;
+    }
+
+   double averageWithdrawal() {    // średnia wartość wypłaty.
+       int n=0;
+       double sum=0;
+       for(double value: transactions) {
+           if (value < 0) {
+               n++;
+               sum += value;
+           }
+       }
+       return sum/n;
+    }
+
+    double averageDeposit() {    // średnia wartość wpłaty.
+        int n=0;
+        double sum=0;
+        for(double value: transactions) {
+            if (value > 0) {
+                n++;
+                sum += value;
+            }
+        }
+        return sum/n;
+    }
+}
+
+

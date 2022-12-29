9e9db4064c9cd6d29ef2c3495a5168f114f941a2
Joanna Pakuła <l.0_0.l@wp.pl>
2022-12-29 14:42:58 +0100

    Initial version

--- kodilla-cucumber/src/main/java/seasonChecker/DivisibilityOfNumbers.java
+++ kodilla-cucumber/src/main/java/seasonChecker/DivisibilityOfNumbers.java
@@ -1,2 +1,19 @@
-package seasonChecker;public class divisibilityOfNumbers {
+package seasonChecker;
+
+public class DivisibilityOfNumbers {
+
+    public static String ifNumberIsDivided(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        else if (number % 3 == 00 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else {
+            return "Nope";
+        }
+    }
 }

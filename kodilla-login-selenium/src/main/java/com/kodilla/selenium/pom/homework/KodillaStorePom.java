a6cd461d54006078d9583e41139fc52c27f1eeea
Joanna Pakuła <l.0_0.l@wp.pl>
2023-01-02 16:50:52 +0100

    Initial version

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaStorePom.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaStorePom.java
@@ -1,35 +1,42 @@
 package com.kodilla.selenium.pom.homework;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 
 public class KodillaStorePom {
 
-    @FindBy(css = "input[type='text']")
-    WebElement text;
+    @FindBy(css = "input[name='search']")
+    WebElement searchField;
 
+   @FindAll({@FindBy(css = "#elements-wrapper > div")})
+    public List<WebElement> results;
     WebDriver driver;
+
+    private int numberOfResults;
 
     public KodillaStorePom(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    public void noteBook(String noteBook) {
-        text.sendKeys(noteBook);
-        text.click();
-    }
 
-    WebDriverWait wait = new WebDriverWait(driver, 20);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("my_Class")));
-
-    public void school(String school) {
-        text.sendKeys(school);
-        text.click();
+    public int enterSth(String search, int numberOfResults) throws InterruptedException {
+        this.numberOfResults = numberOfResults;
+        searchField.clear();
+        searchField.sendKeys(search);
+        Thread.sleep(10000);
+        System.out.println(results.size() == numberOfResults);
+        System.out.println(results.size());
+        return numberOfResults;
     }
 }
+
+
+// zbudować selektor css dla div zawierającego listę innych divów-> stackoverflow
+
+

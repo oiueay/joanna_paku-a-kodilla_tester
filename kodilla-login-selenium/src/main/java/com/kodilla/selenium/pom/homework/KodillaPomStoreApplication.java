96eb43314aa26087e83df5289fe7ce90f1958f96
Joanna Pakuła <l.0_0.l@wp.pl>
2023-01-02 16:51:45 +0100

    Initial version

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaPomStoreApplication.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaPomStoreApplication.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class KodillaPomStoreApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Asus\\IdeaProjects\\kodilla-course\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://kodilla.com/pl/test/store");
+        driver.navigate().to("https://kodilla.com/pl/test/login");
         KodillaStorePom storePom = new KodillaStorePom(driver);
-        storePom.noteBook("NoteBook");
-        storePom.noteBook("school");
-//        driver.close();
+        storePom.enterSth("NoteBook", 2);
+        storePom.enterSth("School", 1);
+        storePom.enterSth("Brand", 1);
+        storePom.enterSth("Business", 0);
+        storePom.enterSth("Gaming", 1);
+        storePom.enterSth("Powerful", 0);
     }
 }
 

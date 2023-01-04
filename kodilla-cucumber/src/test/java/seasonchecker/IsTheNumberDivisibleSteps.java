b9ebda11a79f2902f9426db13f79e61b9274e59a
Joanna Pakuła <l.0_0.l@wp.pl>
2023-01-04 16:20:51 +0100

    Change

--- kodilla-cucumber/src/test/java/seasonchecker/IsTheNumberDivisibleSteps.java
+++ kodilla-cucumber/src/test/java/seasonchecker/IsTheNumberDivisibleSteps.java
@@ -1,2 +1,22 @@
-package seasonchecker;public class IsTheNumberDivisibleSteps {
+package seasonchecker;
+import io.cucumber.java8.En;
+import org.junit.Assert;
+import seasonChecker.DivisibilityOfNumbers;
+
+public class IsTheNumberDivisibleSteps implements En {
+    private int number;
+    private String answer;
+    private String dividedBy;
+
+    public IsTheNumberDivisibleSteps() {
+        Given("a number which is equal to {int}", (Integer int1) -> {
+            this.number = int1;
+        });
+        When("I call {string} algorithm", (String answer) -> {
+            this.answer = "Fizz";
+        });
+        Then("I should be told {string}", (String string) -> {
+            Assert.assertEquals(string, this.answer);
+        });
+    }
 }

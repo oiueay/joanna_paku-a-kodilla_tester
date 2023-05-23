7fcd2e4c2e98981367bbcb72c36f65f550192115
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 15:58:51 +0200

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/DeliveryServiceInterface.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/DeliveryServiceInterface.java
@@ -1,2 +1,6 @@
-package com.kodilla.spring.basic.spring_dependency_injection.homework2;public interface DeliveryServiceInterface {
+package com.kodilla.spring.basic.spring_dependency_injection.homework2;
+
+public interface DeliveryServiceInterface {
+
+    public boolean deliverPackage(String address, double weight);
 }

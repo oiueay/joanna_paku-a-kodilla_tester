d7123b73022acd6cfb1d777c7af4137dabc38cd3
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 17:39:57 +0200

    Initial version

--- kodilla-cucumber/src/main/java/wallet/CashSlot.java
+++ kodilla-cucumber/src/main/java/wallet/CashSlot.java
@@ -1,12 +1,12 @@
-//package wallet;
-//
-//public class CashSlot {
-//
-//    private int contents;
-//    public int getContents() {
-//        return contents;
-//    }
-//    public void dispense(int amount) {
-//        this.contents = amount;
-//    }
-//}
+package wallet;
+
+public class CashSlot {
+
+    private int contents;
+    public int getContents() {
+        return contents;
+    }
+    public void dispense(int amount) {
+        this.contents = amount;
+    }
+}

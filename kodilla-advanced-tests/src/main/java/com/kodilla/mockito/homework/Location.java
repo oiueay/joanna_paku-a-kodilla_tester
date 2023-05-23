0f8a5d23ddbf42cb2e0de8777b957548c370ce06
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:15:27 +0200

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Location.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Location.java
@@ -1,2 +1,20 @@
-package com.kodilla.mockito.homework;public class Location {
+package com.kodilla.mockito.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Location {
+
+    private List<String> locations = new ArrayList<>();
+
+    public Location(List<String> locations) {
+        this.locations = locations;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "locations=" + locations +
+                '}';
+    }
 }

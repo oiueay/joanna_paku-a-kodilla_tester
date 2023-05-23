6e9d9e30d148f7e1df8719cbb9c4ef6ba2cad706
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:50:39 +0200

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
-class StringManipulatorTest {
-  
+package com.kodilla.parametrized_tests;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringManipulatorTest {
+
+    private StringManipulator manipulator = new StringManipulator();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
+    public void shouldReverseStringWithLowerCase(String input, String expected) {
+        assertEquals(expected, manipulator.reverseWithLowerCase(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "provideStringsForTestingLength")
+    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+    }
+
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
+    }
 }

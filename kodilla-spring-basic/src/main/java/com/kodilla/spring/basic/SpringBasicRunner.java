d974027f941c8025d02c890f16680e3935ae0fe3
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:17:41 +0200

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/SpringBasicRunner.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/SpringBasicRunner.java
@@ -1,2 +1,16 @@
-package com.kodilla.spring.basic;public class SpringBasicRunner {
+package com.kodilla.spring.basic;
+
+import com.kodilla.spring.basic.dependency_injection.FacebookMessageService;
+import com.kodilla.spring.basic.dependency_injection.MessageService;
+import com.kodilla.spring.basic.dependency_injection.SimpleApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBasicRunner {
+    public static void main(String[] args) {
+        MessageService messageService = new FacebookMessageService();
+        SimpleApplication application = new SimpleApplication(messageService);
+        application.processMessage("Test message", "receiver@mail.com");
+    }
 }

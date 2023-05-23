f4f643abdb74c1f549c8a6332958154d18463762
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 11:53:03 +0200

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/exception/ThirdExample.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/ThirdExample.java
@@ -1,2 +1,14 @@
-package com.kodilla.stream.exception;public class ThirdExample {
+package com.kodilla.stream.exception;
+
+public class ThirdExample {
+    public static void main(String[] args) {
+        AirportRepository airportRepository = new AirportRepository();
+        boolean isViennaInUse = false;
+        try {
+            isViennaInUse = airportRepository.isAirportInUse("Vienna");
+        } catch (AirportNotFoundException e) {
+            System.out.println("Vienna status: " + isViennaInUse);
+        }
+        System.out.println("Thank you for using Kodilla Airlines");
+    }
 }

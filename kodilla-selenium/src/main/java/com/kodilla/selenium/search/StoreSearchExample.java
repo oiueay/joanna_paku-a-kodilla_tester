5b3b09a417f40c2e828aa22066b6bbd60149b4a0
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:38:30 +0200

    Initial version

--- kodilla-selenium/src/main/java/com/kodilla/selenium/search/StoreSearchExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/search/StoreSearchExample.java
@@ -1,2 +1,23 @@
-package com.kodilla.selenium.search;public class StoreSearchExample {
+package com.kodilla.selenium.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class gStoreSearchExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://kodilla.com/pl/test/store");
+
+        WebElement inputField = driver.findElement(By.name("search"));
+        inputField.sendKeys("School");
+        inputField.submit(); //zasymulowanie wciśnięcia klawisza Enter w kontrolce wyszukiwania
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("my_Class")));
+    }
 }

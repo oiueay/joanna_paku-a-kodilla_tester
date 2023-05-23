e6ee1f7b39b3d4e229810687c6f570b85e865f1f
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 10:14:23 +0200

    Initial version

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -4,13 +4,9 @@ public class InterfacesDemo {
 
     public static void main(String[] args) {
         Square square = new Square(10.0);
-//        System.out.println(square.getArea());
-//        System.out.println(square.getPerimeter());
         showShapeDetails(square);
 
         Circle circle = new Circle(7.0);
-//        System.out.println(circle.getArea());
-//        System.out.println(circle.getPerimeter());
         showShapeDetails(circle);
 
         Triangle triangle = new Triangle(6.0, 4.0, 7.211);

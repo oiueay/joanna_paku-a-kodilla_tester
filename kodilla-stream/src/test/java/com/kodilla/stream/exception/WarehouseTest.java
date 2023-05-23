07687f9fe0210b23127f737968fad3cdb46b3c21
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 12:27:59 +0200

    Initial version

--- kodilla-stream/src/test/java/com/kodilla/stream/exception/WarehouseTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/exception/WarehouseTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class WarehouseTest {
 
     @Test
-    public void mmm_withException() {
+    public void testIsWarehouseInUse_withException() {
         Warehouse warehouse = new Warehouse();
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("1111"));
     }

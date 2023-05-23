3b64688f29d851366c84a188a4aa19d3a10fbece
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:28:38 +0200

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringValidator.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringValidator.java
@@ -1,2 +1,7 @@
-package com.kodilla.parametrized_tests;public class StringValidator {
+package com.kodilla.parametrized_tests;
+
+public class StringValidator {
+    public boolean isBlank(String text) {
+        return text == null || text.trim().isEmpty();
+    }
 }

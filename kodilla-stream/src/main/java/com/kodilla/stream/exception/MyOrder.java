d8d6ba5c5145a4e2ecafdb1aed57248f77b143b1
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 11:58:01 +0200

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/exception/MyOrder.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/MyOrder.java
@@ -1,2 +1,29 @@
-package com.kodilla.stream.exception;public class MyOrder {
+package com.kodilla.stream.exception;
+
+import java.util.Objects;
+
+public class MyOrder {
+
+    private String number;
+
+    public MyOrder(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyOrder myOrder = (MyOrder) o;
+        return Objects.equals(number, myOrder.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }

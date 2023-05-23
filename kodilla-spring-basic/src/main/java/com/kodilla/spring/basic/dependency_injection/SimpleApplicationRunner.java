b0b8ccc38aa48462d63c6fee52ceaf9777bb8b56
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 15:45:22 +0200

    change

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplicationRunner.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplicationRunner.java
@@ -1,2 +1,10 @@
-package com.kodilla.spring.basic.dependency_injection;public class SimpleApplicationRunner {
+package com.kodilla.spring.basic.dependency_injection;
+
+public class SimpleApplicationRunner {
+
+    public static void main(String[] args) {
+        MessageService messageService = new FacebookMessageService();
+        SimpleApplication application = new SimpleApplication(messageService);
+        application.processMessage("Test message", "receiver@mail.com");
+    }
 }

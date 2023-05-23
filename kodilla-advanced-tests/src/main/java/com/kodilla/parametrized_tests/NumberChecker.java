b6a6b1ac4f5704cc278ecf0561643954f54ce9b0
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:25:44 +0200

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/NumberChecker.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/NumberChecker.java
@@ -1,2 +1,8 @@
-package com.kodilla.parametrized_tests;public class NumberChecker {
+package com.kodilla.parametrized_tests;
+
+public class NumberChecker {
+
+    public boolean isDivisibleByThree(int number){
+        return number % 3 == 0;
+    }
 }

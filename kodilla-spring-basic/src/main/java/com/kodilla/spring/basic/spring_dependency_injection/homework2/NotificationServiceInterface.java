c7c758d6df41fc56de1a98e23e154b690e38fea0
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:00:14 +0200

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/NotificationServiceInterface.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/NotificationServiceInterface.java
@@ -1,2 +1,8 @@
-package com.kodilla.spring.basic.spring_dependency_injection.homework2;public interface NotificationServiceInterface {
+package com.kodilla.spring.basic.spring_dependency_injection.homework2;
+
+public interface NotificationServiceInterface {
+
+    public String success(String address);
+
+    public String fail(String address);
 }

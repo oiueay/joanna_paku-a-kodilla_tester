9df55854fbc386e077d3a4b36dc40ad25bf10967
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:14:07 +0200

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Client.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Client.java
@@ -1,2 +1,36 @@
-package com.kodilla.mockito.homework;public class Client {
+package com.kodilla.mockito.homework;
+
+import java.util.Objects;
+
+public class Client {
+
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(name, client.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }

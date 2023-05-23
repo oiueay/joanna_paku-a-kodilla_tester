4eeb54e3efaca33e9311464c778f6c9687997980
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:43:37 +0200

    Initial version

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaPomApplication.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaPomApplication.java
@@ -1,2 +1,16 @@
-package com.kodilla.selenium.pom;public class KodillaPomApplication {
+package com.kodilla.selenium.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KodillaPomApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Asus\\IdeaProjects\\kodilla-course\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://kodilla.com/pl/test/login");
+        KodillaLoginPom loginPom = new KodillaLoginPom(driver);
+        loginPom.login("test@kodilla.com", "haslo");
+        driver.close();
+    }
 }

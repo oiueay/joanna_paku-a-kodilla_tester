fa960c3f4d94f96669fd512485f78815f3f10bda
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:48:29 +0200

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTest.java
@@ -1,4 +1,15 @@
+package com.kodilla.parametrized_tests;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
+
 class StringValidatorTest {
-  
+
+    private StringValidator validator = new StringValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   "})
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        assertTrue(validator.isBlank(text));
+    }
 }

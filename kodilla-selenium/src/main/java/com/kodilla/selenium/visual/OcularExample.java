ba8071a9cbb7a634ffe80c5feb5e0dccaff33f8e
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:39:26 +0200

    Initial version

--- kodilla-selenium/src/main/java/com/kodilla/selenium/visual/OcularExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/visual/OcularExample.java
@@ -1,2 +1,27 @@
-package com.kodilla.selenium.visual;public class OcularExample {
+package com.kodilla.selenium.visual;
+
+import com.testautomationguru.ocular.Ocular;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.file.Paths;
+
+public class OcularExample {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "//c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        Ocular.config()
+                .resultPath(Paths.get("C:\\Users\\Asus\\IdeaProjects\\Ocular\\results"))
+                .snapshotPath(Paths.get("C:\\Users\\Asus\\IdeaProjects\\Ocular\\snapshots"))
+                .globalSimilarity(95)
+                .saveSnapshot(true);
+
+        WorldTimePage page = new WorldTimePage(webDriver);
+
+        page.open();
+        page.close();
+    }
 }
+

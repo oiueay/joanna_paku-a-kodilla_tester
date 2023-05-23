9b03ab2af03fb59e2d435675db281e63ffa88323
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 16:42:29 +0200

    Initial version

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaLoginPom.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaLoginPom.java
@@ -1,2 +1,34 @@
-package com.kodilla.selenium.pom;public class KodillaLoginPom {
+package com.kodilla.selenium.pom;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class KodillaLoginPom {
+
+    @FindBy(css = "input[type='email']")
+    WebElement emailField;
+
+    @FindBy(css = "input[type='password']")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//button[contains(text(), 'Log in')]")
+    WebElement loginButton;
+
+    WebDriver driver;
+
+    public KodillaLoginPom(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean login(String email, String password) {
+        emailField.sendKeys("testuser@gmail.com");
+        passwordField.sendKeys("kodilla123");
+        loginButton.click();
+        String message = driver.switchTo().alert().getText();
+        driver.switchTo().alert().dismiss();
+        return message.equals("Jesteś teraz zalogowany!");
+    }
 }
+

34ce940cd541dc16dd5eb4fdca85dc76cfcf7d27
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:37:44 +0200

    change

--- kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/InvoiceTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/InvoiceTest.java
@@ -1,4 +1,68 @@
-import static org.junit.jupiter.api.Assertions.*;
-class InvoiceTest {
-  
+package com.kodilla.execution_model;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class InvoiceTest {
+
+    Invoice invoice = new Invoice();
+    Item milk = new Item("Milk", 3.50);
+    Item eggs = new Item("Eggs", 8.00);
+    Item bread = new Item("Bread", 4.15);
+
+    @Test
+    public void shouldAddItemsToInvoice() {
+        int numberOfItems = invoice.getSize();
+        assertEquals(3, numberOfItems);
+    }
+
+    @Test
+    public void shouldGetExistingItem() {
+        Item result = invoice.getItem(2);
+        assertEquals("Bread", result.getName());
+        assertEquals(4.15, result.getPrice(), 0.01);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingNegativeIndex() {
+        Item result = invoice.getItem(-3);
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingOutOfRangeIndex() {
+        Item result = invoice.getItem(7);
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldClearInvoice() {
+        invoice.clear();
+        assertEquals(0, invoice.getSize());
+    }
+
+    @BeforeEach
+    public void initializeInvoice() {
+        invoice.addItem(milk);
+        invoice.addItem(eggs);
+        invoice.addItem(bread);
+    }
+
+    @AfterEach
+    public void resetValues() {
+        System.out.println("Resetting values...");
+    }
+
+    @BeforeAll
+    public static void displayIntroMessage() {
+        System.out.println("Starting testing");
+    }
+
+    @AfterAll
+    public static void displayGoodByeMessage() {
+        System.out.println("Finishing testing");
+    }
+
 }

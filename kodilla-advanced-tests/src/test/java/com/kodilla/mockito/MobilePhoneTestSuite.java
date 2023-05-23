c2317eb7f3ac0c4b71013f355eb318cadb2ec7fc
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-23 13:44:28 +0200

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/MobilePhoneTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/MobilePhoneTestSuite.java
@@ -1,2 +1,21 @@
-package com.kodilla.mockito;public class MobilePhoneTestSuite {
+package com.kodilla.mockito;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MobilePhoneTestSuite {
+    MobilePhone myPhone = Mockito.mock(MobilePhone.class);
+
+    @Test
+    public void testExpectation() {
+        Assertions.assertFalse(myPhone.needsCharging());
+        Mockito.when(myPhone.needsCharging()).thenReturn(true);
+        Assertions.assertTrue(myPhone.needsCharging());
+    }
+
+    @Test
+    public void shouldCallLaunchApplication() {
+        myPhone.launchApplication("Tetris4D");
+        Mockito.verify(myPhone).launchApplication("Tetris4D");
+    }
 }

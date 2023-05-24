9402401b582c78121d663273bfe467daacf388d9
Joanna Pakuła <l.0_0.l@wp.pl>
2023-05-24 12:05:51 +0200

    Initial version

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion2/ResultChecker.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion2/ResultChecker.java
@@ -1,12 +1,12 @@
-//package com.kodilla.com.kodilla.basic_assertion2;
-//
-//public class ResultChecker {
-//
-//
-//    public static boolean assertEquals(int expected, int actual) {
-//        return expected == actual;
-//    }
-//    public static boolean assertEquals(double expected, double actual, double delta){
-//        return expected == actual;
-//    }
-//}
+package com.kodilla.basic_assertion2;
+
+public class ResultChecker {
+
+
+    public static boolean assertEquals(int expected, int actual) {
+        return expected == actual;
+    }
+    public static boolean assertEquals(double expected, double actual, double delta){
+        return expected == actual;
+    }
+}
